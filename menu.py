e160f8bc2224525765eb32b4d295fdc22256e2fb
some cunt who flunked C <afonso_rosa@hotmail.com>
2019-01-28 03:50:30 +0000

    fuk me and fuk my code

--- menu.py
+++ menu.py
@@ -2,8 +2,8 @@ import pygame, sys
 from settings import *
 pygame.init()
 
-screen = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
-screen.fill((255,255,255))
+win = pygame.display.set_mode((1280,720))
+win.fill((255,255,255))
 #class do butao de play
 class buttonP():
     def __init__(self, color, x,y,width,height,):
@@ -12,9 +12,6 @@ class buttonP():
         self.y = y
         self.width = width
         self.height = height
-        self.default = pygame.image.load("Sprites/ui/startDefault.png").convert_alpha()
-        self.rect = self.default.get_rect()
-        self.hover = pygame.image.load("Sprites/ui/startHover.png").convert_alpha()
     def draw(self,win,outline = None):
         #call this method to draw the button on the screen
         if outline:
@@ -50,50 +47,50 @@ class buttonE():
 
     #call para redesenhar janela
 def redrawWindow():
-        screen.fill((255,255,255))
+        win.fill((255,255,255))
+        playButton.draw(win)
+        exitButton.draw(win, (0,0,0))
 
-def menu ():
-    run = True
-    #play button
-    playButton = buttonP((0,255,0), 515, 250, 250, 100)#por ordem - cor, X, Y, width, height
-    exitButton = buttonE((0,0,255), 540, 370, 200, 80)#por ordem - cor, X, Y, width, height
+run = True
+#play button
+playButton = buttonP((0,255,0), 515, 250, 250, 100)#por ordem - cor, X, Y, width, height
+exitButton = buttonE((0,0,255), 540, 370, 200, 80)#por ordem - cor, X, Y, width, height
 
-    while run:
-        redrawWindow()
-        pygame.display.update()
+while run:
+    redrawWindow()
+    pygame.display.update()
 
-        for event in pygame.event.get():
-            pos = pygame.mouse.get_pos()
-            #clicks
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                #play button
-                if playButton.isOver(pos):
-                    print('click on start')
-                    screen.blit(playButton.default,playButton.rect)
-                    run = False
-                #exit button
-                if exitButton.isOver(pos):
-                    print('click on exit')
-                    pygame.quit()
-                    sys.exit()    
-                if event.type == pygame.QUIT:
+    for event in pygame.event.get():
+        pos = pygame.mouse.get_pos()
+        #clicks
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            #play button
+            if playButton.isOver(pos):
+                print('click on start')
+                run = False
+            #exit button
+            if exitButton.isOver(pos):
+                print('click on exit')
+                pygame.quit()
+                sys.exit()    
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYUP:
+                if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.KEYUP:
-                    if event.key == K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
-            #hover over
-            if event.type == pygame.MOUSEMOTION:
-                #play button
-                if playButton.isOver(pos):
-                    screen.blit(playButton.default,(515, 250))
-                    print('the button is now red')
-                else:
-                    playButton.color = (0,255,0)
-                #exit button
-                if exitButton.isOver(pos):
-                    exitButton.color = (255,0,0)
-                    print('the button is now red')
-                else:
-                    exitButton.color = (0,0,255)
+        #hover over
+        if event.type == pygame.MOUSEMOTION:
+            #play button
+            if playButton.isOver(pos):
+                playButton.color = (255,0,0)
+                print('the button is now red')
+            else:
+                playButton.color = (0,255,0)
+            #exit button
+            if exitButton.isOver(pos):
+                exitButton.color = (255,0,0)
+                print('the button is now red')
+            else:
+                exitButton.color = (0,0,255)
